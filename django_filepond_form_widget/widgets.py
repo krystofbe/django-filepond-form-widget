112e52477b5cdef420e44d420001442329d8314e
Krystof Beuermann <krystof@blackbox.ms>
2025-01-03 11:51:04 +0100

    feat: enhance FilePond widget with multiple file upload support and improved configuration handling

--- django_filepond_form_widget/widgets.py
+++ django_filepond_form_widget/widgets.py
@@ -5,8 +5,14 @@ class FilePondWidget(forms.ClearableFileInput):
     template_name = "django_filepond_form_widget/filepond_widget.html"
 
     def __init__(self, attrs=None, config=None):
+        # app .filepond-input to the widget attrs class
+        if attrs is None:
+            attrs = {}
+        attrs["class"] = "filepond-input"
+
         super().__init__(attrs)
         self.config = config or {}
+        self.config["storeAsFile"] = True
 
     @property
     def media(self):
@@ -21,12 +27,14 @@ class FilePondWidget(forms.ClearableFileInput):
                 "django_filepond_form_widget/js/filepond-plugin-image-preview.min.js"
             )
 
+        js.append("django_filepond_form_widget/js/init_filepond.js")
+
         return forms.Media(css=css, js=js)
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
         context["widget"]["filepond_config"] = self.config
         context["widget"]["filepond_config_id"] = (
-            f"filepond_{context['widget']['attrs']['id']}"
+            f"filepond_config_{context['widget']['attrs']['id']}"
         )
         return context
